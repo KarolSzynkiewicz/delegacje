df2d5f559b0dc537b8526fcba065fef5e7e194ae
Karol Szynkiewicz <lolis103@gmail.com>
2026-01-13 23:33:13 +0100

    Naprawa: przywrócenie guzików akcji w tabelach przypisań i rotacji
    Komponent x-ui.action-buttons nie obsługiwał parametrów viewRoute, editRoute, deleteRoute.
    Zaktualizowano komponent aby obsługiwał oba tryby: z parametrami route oraz z slotem.

--- resources/views/components/ui/action-buttons.blade.php
+++ resources/views/components/ui/action-buttons.blade.php
@@ -1,8 +1,44 @@
 @props([
+    'viewRoute' => null,
+    'editRoute' => null,
+    'deleteRoute' => null,
+    'deleteMessage' => 'Czy na pewno chcesz usunąć ten element?',
+    'size' => 'sm', // sm, null, lg
     'gap' => '1',
     'class' => '',
 ])
 
-<div class="d-flex gap-{{ $gap }} {{ $class }}">
-    {{ $slot }}
-</div>
+@if($viewRoute || $editRoute || $deleteRoute)
+    {{-- Tryb z parametrami route (backward compatible z action-buttons) --}}
+    <div class="btn-group" role="group">
+        @if($viewRoute)
+            <x-ui.button variant="ghost" href="{{ $viewRoute }}" title="Zobacz">
+                <i class="bi bi-eye"></i>
+            </x-ui.button>
+        @endif
+        
+        @if($editRoute)
+            <x-ui.button variant="ghost" href="{{ $editRoute }}" title="Edytuj">
+                <i class="bi bi-pencil"></i>
+            </x-ui.button>
+        @endif
+        
+        @if($deleteRoute)
+            <form action="{{ $deleteRoute }}" 
+                  method="POST" 
+                  class="d-inline"
+                  onsubmit="return confirm('{{ $deleteMessage }}')">
+                @csrf
+                @method('DELETE')
+                <x-ui.button variant="danger" type="submit" title="Usuń">
+                    <i class="bi bi-trash"></i>
+                </x-ui.button>
+            </form>
+        @endif
+    </div>
+@else
+    {{-- Tryb z slotem (dla custom przycisków) --}}
+    <div class="d-flex gap-{{ $gap }} {{ $class }}">
+        {{ $slot }}
+    </div>
+@endif
