2f1be95cbc3c387f1a95bb96777ff67f45762624
Karol Szynkiewicz <lolis103@gmail.com>
2026-02-03 14:03:25 +0100

    Fix: use envsubst for PORT substitution in minimal entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,10 @@
 # Minimal entrypoint - only start PHP-FPM and Nginx
 # Railway needs PID 1 to be Nginx
 
+# Substitute PORT in nginx config
+export PORT=${PORT:-80}
+envsubst '${PORT}' < /etc/nginx/sites-available/default > /tmp/nginx.conf && mv /tmp/nginx.conf /etc/nginx/sites-available/default
+
 # Start PHP-FPM in background
 php-fpm -D
 
